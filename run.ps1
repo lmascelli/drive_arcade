64ca9d63fcc09933fcc972f572cd388ef1f5efe0
Retro Ginocchio <rgtwitch01@gmail.com>
2022-06-20 11:02:33 +0200

    merge

--- run.ps1
+++ run.ps1
@@ -1,8 +1,20 @@
 Clear-Host
 
-Push-Location build
-cmake .. -G"Ninja"
-cmake --build .
-Move-Item -Force compile_commands.json ..
-./game
-Pop-Location
+function build()
+{
+  Push-Location build
+  cmake .. -G"Ninja"
+  cmake --build .
+  Move-Item -Force compile_commands.json ..
+  ./game
+  Pop-Location
+}
+if (Test-Path -Path "build")
+{
+  build
+} else
+{
+  New-Item -Type Directory -Name "build"
+  build
+}
+
